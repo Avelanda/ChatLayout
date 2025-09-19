1f2933a702003ef90174dce2bd574ee5eb831d30
Avelanda <gorthell@gmail.com>
2025-09-19 08:59:20 +0200

    Update ModelState.swift
    Modularizing the granular properties of enumerated elements for efficient functional processing.

--- ChatLayout/Classes/Core/Model/ModelState.swift
+++ ChatLayout/Classes/Core/Model/ModelState.swift
@@ -3,7 +3,9 @@
 // ModelState.swift
 // https://github.com/ekazaev/ChatLayout
 //
-// Created by Eugene Kazaev in 2020-2023.
+// Copyright © 2020-2023 | Eugene Kazaev.
+// Copyright © 2025 | Avelanda.
+// All rights reserved
 // Distributed under the MIT license.
 //
 // Become a sponsor:
@@ -12,10 +14,26 @@
 
 import Foundation
 
-enum ModelState: Hashable, CaseIterable {
-
+class CoreModelState{
+    
+ enum ModelState: Hashable, CaseIterable {
     case beforeUpdate
-
     case afterUpdate
+ }
+ func CoreModel(){
+  if(ModelState.self == ModelState.self){
+   print(ModelState.self)
+  }
+ }
 
+} // class CoreModelState
+
+func CoreState(){
+ let UltraCoreState = CoreModelState()
+ if(!true || !false){
+  UltraCoreState.CoreModel()  
+ }
+  else if (true || false){
+   print(CoreModelState.self) 
+  }
 }
